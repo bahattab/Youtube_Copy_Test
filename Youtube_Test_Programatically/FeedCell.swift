eae2eb4b969e8ed7edfda275e6baa7b7dac6ddf8
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-07-19 12:28:41 +0200

    Add Collection view to FeedCell

--- Youtube_Test_Programatically/FeedCell.swift
+++ Youtube_Test_Programatically/FeedCell.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class FeedCell: BaseCell {
+class FeedCell: BaseCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    let collectionView: UICollectionView = {
+    lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = UIColor.whiteColor()
+        cv.dataSource = self
+        cv.delegate = self
         return cv
     }()
+    
+    let cellId = "cellId"
     
     override func setUpViews() {
         super.setUpViews()
@@ -23,5 +27,17 @@ class FeedCell: BaseCell {
         addSubview(collectionView)
         addConstrainstWithFormat("H:|[v0]|", views: collectionView)
         addConstrainstWithFormat("V:|[v0]|", views: collectionView)
+        
+        collectionView.registerClass(VideoCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath)
+        
+        return cell
     }
 }
